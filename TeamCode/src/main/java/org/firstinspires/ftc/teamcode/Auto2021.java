5585437032559b44f138e64ec27a8ac4507aa185
BuildTools <apaquet37@charlottesvilleschools.org>
2021-02-20 09:21:23 -0500

    Drive straight

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto2021.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto2021.java
@@ -245,7 +245,15 @@ public class Auto2021 extends LinearOpMode {
             sleep(300);
             driveStraightTime(.3, robot.imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES),1500 );
             sleep(300);*/
-            driveStraightDistance(0.3, robot.imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES), 860 );
+            //driveStraightDistance(0.3, robot.imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES), 860 );
+
+            while (lastTime < 3000){
+                robot.backLeftMotor.setPower(.5);
+                robot.frontRightMotor.setPower(.5);
+                robot.frontLeftMotor.setPower(.5);
+                robot.backRightMotor.setPower(.5);
+            }
+
             while (lastTime < 1000){
                 robot.wobbleMotor.setPower(.25);
             }
